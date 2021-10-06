f95a4b3645581e99a142325a76fe8a992807299f
Koichi Shiraishi <zchee.io@gmail.com>
2021-10-06 12:29:49 +0900

    vsock: split constants
    Signed-off-by: Koichi Shiraishi <zchee.io@gmail.com>

--- vsock/vsock.go
+++ vsock/vsock.go
@@ -7,28 +7,6 @@ import (
 	"fmt"
 	"net"
 	"os"
-
-	"golang.org/x/sys/unix"
-)
-
-// list of context ID.
-const (
-	// CIDAny binds any guest’s context ID. This seems to work inside VMs only.
-	CIDAny = unix.VMADDR_CID_ANY
-
-	// CIDHost well-known host context ID.
-	CIDHost = unix.VMADDR_CID_HOST
-
-	// CIDHypervisor reserved hypervisor context ID.
-	CIDHypervisor = unix.VMADDR_CID_HYPERVISOR
-
-	// CIDReserved reserved guest’s context ID. This must not be used
-	CIDReserved = unix.VMADDR_CID_RESERVED
-)
-
-const (
-	// PortAny binds a random available port.
-	PortAny = unix.VMADDR_PORT_ANY
 )
 
 // Conn represents a vsock connection which supported half close.
